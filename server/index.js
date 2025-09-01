e7a520f7260e910d9fa088fa291d1bf52e1aca3b
Payal Kharb <kharbpayal@gmail.com>
2025-09-01 18:23:16 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://quickfix-lvch.onrender.com"], // your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
